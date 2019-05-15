4e52d5b87c181c2cf318e11062c99696f8d995be
Adil <adelmusse@gmail.com>
2019-05-15 14:12:09 +0800

    conditional code

---
+++ example_1.rb
@@ -0,0 +1,22 @@
+# def say(words)
+#     puts words
+# end
+# say("hello")
+# # ****************************************************
+# def say(word='hello')
+#     puts words
+# end
+# say()
+# # ****************************************************
+def say(number)
+    num2 = number*2
+    puts num2
+end
+    say(8)
+# ******************************************
+# def add_three(number)
+#     number + 4
+# end
+
+# total= add_three(3)
+# puts totalexample_1.rb
