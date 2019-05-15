c7eeca246c53bfe4e18bc22bfd831e267d633b41
Adil Musse <45510790+AdelMusse@users.noreply.github.com>
2019-05-15 23:22:02 +0800

    exercise1 part3
    add extra input for user's gender
    add conditionals so that it will check your gender and your age and giving different greeting depending on whether male or female and age 
    adding output that tells user if their age is even or odd
    finding out what is year of birth assuming that we are in 2019
    and if their year is leap year or not

--- exercise_1.rb
+++ exercise_1.rb
@@ -5,45 +5,32 @@ last_name = gets.chomp
 full_name= first_name+" "+last_name
 puts "hello " full_name
 
-# puts "What is you gender"
-# gender = gets.chomp.downcase
+#********** asking user to input gender and age**********#
+
+puts "What is you gender"
+gender = gets.gsub(/[^a-zA-Z0-9\-]/,"").downcase
 puts  "Enter your age " 
 age = gets.to_i
 
-# greeting = case
-# when gender == "male" && age < 21
-#     "hello kid"
-# when gender == "male" && age >= 22 && age<=40
-#     "hello sir"
-# when gender == "male" && age > 41
-#     "hello pop"
-# when gender == "female" && age < 21
-#     "hello kid"
-# when gender == "female" && age >= 22 && age<=40
-#     "hello ms"
-# when gender == "female" && age > 41
-#     "hello lady"
-# else 
-#      "invalid"
-# end
-# puts greeting
+#************ greeting system ***********#
 
-# if age.even?
-#     puts "your age is an even number"
-# elsif age.odd?
-#     puts "your age is an odd number"
-# end
-
-# puts "your birthday year is #{date_of_birth = 2019-age}"
-
-# case
-# when date_of_birth % 4 == 0
-#     puts "your birth year is a leap year"
-# else
-#     puts "your birth year is not a leap year"
-# end
-
-#******************
+greeting = case
+when gender == "male" && age < 21
+    "hello kid"
+when gender == "male" && age && (22..40)===age
+    "hello sir"
+when gender == "male" && age > 41
+    "hello pop"
+when gender == "female" && age < 21
+    "hello kid"
+when gender == "female" && age && (22..40)===age
+    "hello ms"
+when gender == "female" && age > 41
+    "hello lady"
+else 
+     "invalid"
+end
+puts greeting
 
 puts "your age will be #{age + 10} in 10 years"
 puts "your age will be #{age + 20} in 20 years"
@@ -52,4 +39,24 @@ puts "your age will be #{age + 40} in 40 years"
 puts "your full name reversed is #{full_name.reverse}"
 puts "your full name has #{full_name.length}"
 puts "you full name in uppercase is #{full_name.upcase}"
+
+#****** telling user if their age  is an even or odd number******#
+
+if age.even?
+    puts "your age is an even number"
+elsif age.odd?
+    puts "your age is an odd number"
+end
+
+puts "your birthday year is #{date_of_birth = 2019-age}"
+
+case
+when date_of_birth % 4 == 0
+    puts "your birth year is a leap year"
+else
+    puts "your birth year is not a leap year"
+end
+
+
+
 
